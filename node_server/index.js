81d698e9ca92abb4e7dcb68ea50ab33e518a4005
seb <sebastian.mititelu@gmail.com>
2020-11-02 21:47:25 +0000

    first commit, add environment for d3

---
+++ node_server/index.js
@@ -0,0 +1,3 @@
+var p = document.createElement("p"); //Create a new "p" element (a paragraph)
+p.innerHTML = "Hello, world!"; //Add the text "Hello, world!" to the new paragraph
+document.body.appendChild(p); //Add the paragraph to the page
